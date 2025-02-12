70b600adb7e8559c6c3bc2c3e88f9d5f00678b27
preyas <preyas.ptl2626@gmail.com>
2025-02-12 15:28:30 +0530

    - Initial Commit

---
+++ LocalNotificationDemo/LocalNotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationDemoApp.swift
+//  LocalNotificationDemo
+//
+//  Created by mymac on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
